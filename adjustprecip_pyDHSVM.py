4a7fb78c0704549c5c983590b60aacd5e391f794
keckje <r97521326@gmail.com>
2018-04-10 09:45:03 +0800

    code now runs and creates csv file with adjusted precip

--- adjustprecip_pyDHSVM.py
+++ adjustprecip_pyDHSVM.py
@@ -4,26 +4,31 @@ Created on Mon Mar 26 12:36:37 2018
 
 @author: cband
 """
-import numpy as np
+import os
 import pandas as pd
+import numpy as np
+
+#import numpy as np
+#import pandas as pd
 #Read in forcing file
+
+homedir='D:/UW_PhD/PreeventsProject/Hydrometeorology and Floods/ForcingData/HydrologySensitivityForcingData'
+homedir='D:\GoogleDrive\Courses\Fluvial_Geomorph\Skokomish\Skmh_calcs'
 header=['Date','T_min','T_max','Relative_Humidity','ShortWave','LongWave','Precip_m']
-data=pd.read_csv('C:\Users\cband\Skagit\data_48.34375_-121.34375',sep=' ',names=header)
+data=pd.read_csv('C:/Users/jisheng/repos/pyDHSVM/test.csv',sep=' ',names=header)
 T=(data.T_min+data.T_max)/2
 P=data.Precip_m
 RH=data.Relative_Humidity
 T.shape
 z=np.repeat(167,T.shape[0])
 plapse=0.0000006
-Precip_z = []
+Precip_z = np.zeros(shape = (len(P),1))
 
-for i in range(len(P)):
-    if P > 0.0:                 
-        Precip_z=P + plapse*(z)
+for i in range (len(P)):
+    if P[i] > 0.0:                 
+        Precip_z[i]=P[i] + plapse*(z[i])
     else:
-        Precip_z=P
-
-
+        Precip_z[i]=P[i]
 
 #print some combination of old and new data
 data_lapsed=pd.DataFrame()
@@ -34,4 +39,6 @@ data_lapsed['Relative_Humidity']=np.around(data.Relative_Humidity.values,1)
 data_lapsed['ShortWave']=np.around(data.ShortWave.values,3)
 data_lapsed['LongWave']=np.around(data.LongWave.values,3)
 data_lapsed['Precip_m']=np.around(Precip_z,5)
-data_lapsed.to_csv('sauk_base_station.data_48.34375_-121.34375_test',index=False,header=None,sep=' ')
+
+os.chdir(homedir)
+data_lapsed.to_csv('sauk_base_station.data_48.34375_-121.34375_test.csv',index=False,header=None,sep=' ')
